a828210226d48ac7a3bad2f48b92a458670d0cdf
shenningsong <47602775+shenningsong@users.noreply.github.com>
2019-03-13 21:55:27 +0800

    making linklist

--- ProgramDesignHomework/utils/linkedlist.c
+++ ProgramDesignHomework/utils/linkedlist.c
@@ -1,2 +1,5 @@
 #include "linkedlist.h"
-
+typedef struct  data{
+  struct node *next;
+  void* data;
+}ComponentList;
--- ProgramDesignHomework/utils/linkedlist.h
+++ ProgramDesignHomework/utils/linkedlist.h
@@ -1,1 +1,3 @@
 #pragma once
+void* CreateLinkedList();
+// void* FindInLinkedList(function);
