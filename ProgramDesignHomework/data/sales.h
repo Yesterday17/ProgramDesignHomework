d27a30a2172867bdbfa303b508a2e1b5ac47e9b8
gengtr <1291714393@qq.com>
2019-03-13 15:59:47 +0800

    finish  struct sales

--- ProgramDesignHomework/data/sales.h
+++ ProgramDesignHomework/data/sales.h
@@ -1,1 +1,15 @@
 #pragma once
+#include "sales.h"
+#include"time.h"
+#include"component.h"
+
+
+typedef struct sales {
+	Time time;
+	Component component;
+	int price;       // 销售单价
+	int quantity;    // 销售数量
+	int total;      // 商品总价
+	char *customer; //客户信息
+	Component gift; //赠品
+}Sales;
