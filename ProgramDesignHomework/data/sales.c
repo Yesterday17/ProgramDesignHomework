8c02d1d72a257e76d67950939fefe13358b1dcf3
shenningsong <47602775+shenningsong@users.noreply.github.com>
2019-03-13 21:33:15 +0800

    change scanf %s

--- ProgramDesignHomework/data/sales.c
+++ ProgramDesignHomework/data/sales.c
@@ -3,10 +3,12 @@
 Sales ReadSales() {
   Sales prime;
   prime.time = ReadTime();
+  printf("批发/零售价格：");  //批发零售价格
   scanf("%d", &prime.price);
+  printf("批发/零售数量：");//批发/零售数量：
   scanf("%d", &prime.quantity);
   prime.total = prime.price * prime.quantity;
-  scanf("%s", &prime.customer);
+  prime.customer=InputString("please input customer：", "3021 bosses");
   ReadComponent();
   return prime;
 }
