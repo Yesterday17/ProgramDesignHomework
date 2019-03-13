9a2afba233b26e72220e362da9326eb3ae620844
cuihr2118 <54208051@qq.com>
2019-03-13 16:26:24 +0800

    finish time.c

--- ProgramDesignHomework/data/time.c
+++ ProgramDesignHomework/data/time.c
@@ -3,12 +3,12 @@
 Time ReadTime() {
 	Time t0;
 	printf("Month: ");
-	scanf("%s", t0.month);
+	scanf("%d", &t0.month);
 	printf("\nDay: ");
-	scanf("%s", t0.day);
+	scanf("%d", &t0.day);
 	printf("/nHour: ");
-	scanf("%s", t0.hour);
+	scanf("%d", &t0.hour);
 	printf("/nMinute: ");
-	scanf("%s", t0.minute);
+	scanf("%d", &t0.minute);
 	return t0;
 }
