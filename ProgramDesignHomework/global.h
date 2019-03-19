980a9f7865ca1b2dc2c72d08e62851bb1361af1b
Yesterday17 <t@yesterday17.cn>
2019-03-19 22:25:46 +0800

    Fix broken CI.

--- ProgramDesignHomework/global.h
+++ ProgramDesignHomework/global.h
@@ -1,12 +1,17 @@
 #pragma once
 #include <stdint.h>
-#include "./ui/ui.h"
 #include "./data/component.h"
+#include "./ui/ui.h"
+#include "./utils/linkedlist.h"
 
 extern const char NAME[];
 extern const char VERSION[];
 
 extern Menu menuNow;
+
+LinkedList component;
+LinkedList purchase;
+LinkedList sales;
 
 extern char* nameToSearch;
 extern uint64_t timeToSearch;
