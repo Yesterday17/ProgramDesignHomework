230c4b73c43b73bf3c5be8077d89468416856ec0
gengtr <1291714393@qq.com>
2019-03-20 16:30:47 +0800

    submit ui

--- ProgramDesignHomework/ui/ui.c
+++ ProgramDesignHomework/ui/ui.c
@@ -4,7 +4,63 @@ void UI_Welcome() {}
 
 void UI_MainMenu() {}
 
-void UI_SubMenu(Menu menu) {}
+void UI_SubMenu(Menu menu)
+{
+	char ch;
+	if (menu == MENU_) {
+		printf("1.¶ÁÈ¡\n");
+		printf("2.Ìí¼Ó\n");
+		printf("3.É¾³ý\n");
+		ch = getch();
+		switch (ch)
+		{
+			case '1':
+				
+				system("cls");
+				break;
+			case '2':
+				
+				system("cls"); 
+				break;
+			case '3':
+				
+				system("cls"); 
+				break;
+			case '4':
+				
+				system("cls"); 
+				break;
+		}
+	}
+	if (menu == MENU_) {
+		switch (ch)
+		{
+		case '1':break;
+		case '2':break;
+		case '3':break;
+		case '4':break;
+		}
+	}
+	if (menu == MENU_) {
+		switch (ch)
+		{
+		case '1':break;
+		case '2':break;
+		case '3':break;
+		case '4':break;
+		}
+	}
+	if (menu == MENU_) {
+		switch (ch)
+		{
+		case '1':break;
+		case '2':break;
+		case '3':break;
+		case '4':break;
+		}
+	}
+
+}
 
 void UI_Exit() {}
 
