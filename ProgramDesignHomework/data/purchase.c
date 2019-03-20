3382ce544618398552e6e9e46eb42048c2401330
shenningsong <47602775+shenningsong@users.noreply.github.com>
2019-03-20 14:16:35 +0800

    change scanf

--- ProgramDesignHomework/data/purchase.c
+++ ProgramDesignHomework/data/purchase.c
@@ -6,8 +6,8 @@
 Purchase ReadPurchase() {
   Purchase prime;
   prime.time = ReadTime();
-  scanf("%d", &prime.price);                   //输入进货单价
-  scanf("%d", &prime.quantity);                //输入进货数量
+  prime.price = InputString("请输入进货单价","1");//输入进货单价
+  prime.quantity=InputString("请输入进货数量","1");              //输入进货数量
   prime.total = prime.price * prime.quantity;  //输入商品总价
   prime.retailer =
       InputString("please input retailer", "3021");  //输入进货供应商
