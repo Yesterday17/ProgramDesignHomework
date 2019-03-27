74c7584d3af1256b94002167f0703a67b4e5d16b
shenningsong <47602775+shenningsong@users.noreply.github.com>
2019-03-27 14:17:46 +0800

    read component struct

--- ProgramDesignHomework/data/component.c
+++ ProgramDesignHomework/data/component.c
@@ -24,5 +24,37 @@ bool FindName_Component(LinkedListNode *node) {
 Component* read_structcomponent(cJSON *item);
 {
 	Component* comp;
+	cJSON *item;
+	cJSON *object = cJSON_GetObjectItem(root, "prime");
+	if (object == NULL)
+	{
+		printf("Error before: [%s]\n", cJSON_GetErrorPtr());
+		cJSON_Delete(root);
+		return -1;
+	}
 
+	if (object != NULL)
+	{
+		item = cJSON_GetObjectItem(object, "index");
+		if (item != NULL)
+		{
+			comp->index = item->valueint;
+		}
+		item = cJSON_GetObjectItem(object, "name");
+		if (item != NULL)
+		{
+			memcpy(comp->name, item->name, strlen(comp->name));
+		}
+		item = cJSON_GetObjectItem(object, "type");
+		if (item != NULL)
+		{
+			memcpy(comp->type, item->type, strlen(comp->type));
+		}
+		item = cJSON_GetObjectItem(object, "manufacturer");
+		if (item != NULL)
+		{
+			memcpy(comp->manufacturer, item->manufacturer, strlen(comp->manufacturer));
+		}
+	}
+	return comp;
 }
