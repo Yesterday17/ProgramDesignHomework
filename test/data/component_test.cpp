bd9b57e36ec86e29a0df93bbd3baa914b0b40c73
Yesterday17 <t@yesterday17.cn>
2019-04-12 00:45:21 +0800

    CreateComponentStruct test.

--- test/data/component_test.cpp
+++ test/data/component_test.cpp
@@ -1,4 +1,5 @@
 #include "gtest/gtest.h"
+#include <stdlib.h>
 
 extern "C" {
 #include "../../ProgramDesignHomework/data/component.h"
@@ -7,7 +8,16 @@ extern "C" {
 /**
  * 测试结构体创建
  */
-TEST(TestComponent, Struct) {
-  // TODO: Test case here.
-  EXPECT_EQ(1, 1);
+TEST(TestComponent, CreateStruct) {
+  Component* comp = (Component*)malloc(sizeof(Component));
+  comp->index = 0;
+  comp->manufacturer = LITERAL("Yesterday17");
+  comp->name = LITERAL("SEN-PAI");
+  comp->type = LITERAL("114-514");
+
+  EXPECT_EQ(comp->index, 0);
+  EXPECT_EQ(compareString(comp->manufacturer, LITERAL("Yesterday17")), STRING_EQUAL);
+  EXPECT_EQ(compareString(comp->name, LITERAL("SEN-PAI")), STRING_EQUAL);
+  EXPECT_EQ(compareString(comp->type, LITERAL("114-514")), STRING_EQUAL);
+  free(comp);
 }
