1912a30ab217a9aa2627038146bd9c8c4a659330
Yesterday17 <t@yesterday17.cn>
2019-04-16 19:44:18 +0800

    Remove fread return value check.

--- ProgramDesignHomework/utils/fs.c
+++ ProgramDesignHomework/utils/fs.c
@@ -10,8 +10,8 @@ bool FileExist(string fileName) { return (_access(CSTR(fileName), 04) != -1); }
 
 // https://stackoverflow.com/questions/3747086/reading-the-whole-text-file-into-a-char-array-in-c
 string ReadFile(string filename) {
-  FILE *fp = fopen(U8_CSTR(filename), "r");
-  if (!fp) {
+  FILE *fp;
+  if (fopen_s(&fp, U8_CSTR(filename), "r") != 0) {
     perror("Failed to open file!");
     exit(1);
   }
@@ -24,11 +24,8 @@ string ReadFile(string filename) {
 
   char* str = calloc(1, fileSize + 1);
 
-  if (fread(str, fileSize, 1, fp) != 0u) {
-    fclose(fp);
-    fputs("entire read fails", stderr);
-    exit(1);
-  }
+  // auto numread = fread_s(str, fileSize, fileSize, 1, fp);
+  fread(str, fileSize, 1, fp);
   string ans = newString(str);
 
   fclose(fp);
